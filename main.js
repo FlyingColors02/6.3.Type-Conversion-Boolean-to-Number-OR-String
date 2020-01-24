254ec69bf30028c2ede35ad3356e2a918d2b6186
FlyingColors02 <shraduborse97@gmail.com>
2020-01-24 21:22:23 +0530

    #shraddha borse: convert boolean to number or string

---
+++ main.js
@@ -0,0 +1,32 @@
+//convert Boolean Value to Number OR String
+
+let boolValue1=true;
+console.log(boolValue1);
+console.log(typeof(boolValue1));
+
+let boolValue2=false;
+console.log(boolValue2);
+console.log(typeof(boolValue2));
+
+
+//convert boolean value to Number
+let numValue1=Number(boolValue1);
+console.log((numValue1));
+console.log(typeof(numValue1));
+//as 0=false and 1=true we get 1 as number value
+
+
+let numValue2=Number(boolValue2);
+console.log((numValue2));
+console.log(typeof(numValue2));
+//as 0=false and 1=tue we get 0 here as number value 
+
+
+//convert boolean to String value
+let strValue1=String(boolValue1);
+console.log((strValue1));
+console.log(typeof(strValue1));
+
+let strValue2=String(boolValue2);
+console.log((strValue2));
+console.log(typeof(strValue2));
